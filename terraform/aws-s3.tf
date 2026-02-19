1393c40d4d32bc8cf4bc77940f447b0df29f4a99
Kai O'Brien <kaiobrien@Kais-MacBook-Pro.local>
2026-02-19 20:34:47 +0100

    added basic config for aws-s3.tf

---
+++ terraform/aws-s3.tf
@@ -0,0 +1,44 @@
+module "s3_raw_data" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 4.0"
+
+  bucket = "customer-churn-raw-data"
+  force_destroy = true
+
+  control_object_ownership = true
+  object_ownership = "BucketOwnerEnforced"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+module "s3_processed_data" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 4.0"
+
+  bucket = "customer-churn-processed-data"
+  force_destroy = true
+
+  control_object_ownership = true
+  object_ownership = "BucketOwnerEnforced"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+module "s3_model_artifacts" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 4.0"
+
+  bucket = "customer-churn-model-artifacts"
+  force_destroy = true
+
+  control_object_ownership = true
+  object_ownership = "BucketOwnerEnforced"
+
+  versioning = {
+    enabled = true
+  }
+}
